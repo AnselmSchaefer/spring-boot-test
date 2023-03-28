3b47f28cac1e6853ce959d861260c27579f50896
Anselm Schaefer <schaefab@basfad.basf.net>
2023-03-28 23:51:32 +0200

    JDBCUserDetailsManager

--- src/main/java/org/springbootbuch/springboottest/config/ProjectConfig.java
+++ src/main/java/org/springbootbuch/springboottest/config/ProjectConfig.java
@@ -1,22 +1,25 @@
 package org.springbootbuch.springboottest.config;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails u = new User("john", "1234", "read");
-		List<UserDetails> users = List.of(u);
-		return new InMemoryUserDetailsService(users);
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
+	    String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+	    var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+	    userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+	    userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+	    return userDetailsManager;
 	}
 	
 	@Bean
